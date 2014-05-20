e758b0c8420dd5cd75de5c53dbc84115214ebaa5
Sakshi Jain <s28jain@uwaterloo.ca>
2014-05-20 16:25:04 -0400

    Added basic calls to create a map

---
+++ map.py
@@ -0,0 +1,10 @@
+import sys
+import pygame
+
+
+class Map:
+        def __init__(self, grid_size):
+            self.grid_size = grid_size
+
+        def addOrReplaceTile(self, tile, v_loc, h_loc):
+            pass
